dd5dccfb9b7ed6bf1a83b5537126bdb51d683496
eschroer <schroer.emerson@gmail.com>
2021-09-10 17:07:13 -0500

    Tank Info Component

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,15 +1,74 @@
-import { NavigationHelpersContext } from "@react-navigation/core";
-import React, {useState} from "react";
-import { View, Text, FlatList } from 'react-native';
-import { Tile } from 'react-native-elements'
+import React, {useEffect, useState } from "react";
+import { View, Text, FlatList, Modal, StyleSheet } from 'react-native';
+import { Tile, FAB, Input, Button } from 'react-native-elements'
 import { AQUARIUMS } from "../shared/aquariums";
-import { useNavigation } from '@react-navigation/native'
+import { useNavigation } from '@react-navigation/native';
+import { AsyncStorage } from 'react-native';
 
-export default function Main() {
+
+
+function Main() {
     const [aquariums, manageAquariums] = useState(AQUARIUMS)
-    const navigation = useNavigation();
+    const [modalVisible, setModalVisible] = useState(false)
+    const [enteredTitle, setEnteredTitle] = useState('');
+    const [enteredSize, setEnteredSize] = useState('')
     
+    const navigation = useNavigation();
+    let newAquariums = {
+        name: enteredTitle,
+        id: aquariums.length,
+        size: enteredSize
+    }
+    
+    // console.log(aquariums)
+    // useEffect(() => {
+    //     getData();
+    //   }, []);
+    
+    //   useEffect(() => {
+    //     setData();
+    //   }, [aquariums]);
+
+    //   const setData = async (aquariums) => {
+    //     try {
+    //       await AsyncStorage.setItem("aquariums", JSON.stringify(aquariums));
+    //     } catch (error) {
+    //       console.log(error);
+    //     }
+    //   };
+
+    //   const getData = async () => {
+    //     try {
+    //       const value = await AsyncStorage.getItem("aquariums") 
+    //         if (value != null) {
+    //           let parsedValue = JSON.parse(value);
+    //           manageAquariums(aquariums.push(parsedValue));
+    //         }
+    //     } catch (error) {
+    //       console.log(error);
+    //     }
+    //   };
+    const titleChangeHandler = (enteredTitle) => {
+        setEnteredTitle(enteredTitle)
+    }
+
+    const subtitleChangeHandler = (enteredSize) => {
+        setEnteredSize(enteredSize)
+    }
+
+    function addAquarium() {
+   
+        manageAquariums(prevAquariums => {
+            return newAquariums = [...prevAquariums, newAquariums]
+        })
+         setEnteredTitle('');
+         setEnteredSize('');
+        // return newAquariums;
+    }
+      
+       
     const renderAquarium = ({item}) => {
+         
         
         return(
             <View>
@@ -18,21 +77,92 @@ export default function Main() {
                     caption={item.size}
                     featured
                     imageSrc= {{uri: item.image}}
-                    onPress={() => navigation.navigate('TankInfo')}
+                    onPress={() => navigation.navigate('TankInfo', {aquariumId: item.id, newAquarium: newAquariums})}
                 />
                 
             </View>
         )
     }
-    
+
+
     return(
+       
         <View>
             <FlatList
                 data={aquariums}
                 renderItem={renderAquarium}
                 keyExtractor={item => item.id.toString()}
             />
+            <Modal
+                animationType="slide"
+                transparent={false}
+                visible={modalVisible}
+                onRequestClose={() => {
+                  setModalVisible(!modalVisible);
+                }} 
+            >
+                <View style={styles.modal}>
+                    <Text style={styles.modalTitle}>Add Aquarium</Text>
+                </View> 
+                <Input
+                    placeholder="Name"
+                    onChangeText={titleChangeHandler}
+                    value={enteredTitle} 
+                />
+                <Input
+                    placeholder="Tank Size"
+                    onChangeText={subtitleChangeHandler}
+                    value={enteredSize} 
+                />
+                <Button
+                    onPress={() => {
+                        addAquarium()
+                        setModalVisible(!modalVisible)
+                    }}
+                    title="Submit"
+                />
+                <Button
+                    onPress={() => {
+                        setModalVisible(!modalVisible)
+                    }}
+                    title="Cancel"
+                />
+            </Modal>
+
+            <FAB
+            title="+"
+            placement="right"
+            color="#045de9"
+            onPress={()=> setModalVisible(!modalVisible)}
+            />
+       
         </View>
+    
+       
+
     )
 }
 
+const styles = StyleSheet.create({
+    modal: {
+        justifyContent: 'center',
+        margin: 20,
+        backgroundColor: '#045de9'
+    },
+    modalTitle: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        backgroundColor: '#045de9',
+        textAlign: 'center',
+        color: '#fff',
+        marginTop: 20,
+        marginBottom: 20
+    },
+    modalText: {
+        fontSize: 18,
+        margin: 10
+    }
+
+})
+
+export default Main; 
--- components/TankInfoComponent.js
+++ components/TankInfoComponent.js
@@ -1,15 +1,48 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Text, View } from 'react-native';
 import { Card } from 'react-native-elements';
 import { AQUARIUMS } from '../shared/aquariums';
 
 
-export default function TankInfo() {
+export default function TankInfo({route, navigation, }) {
+    const [aquariumData, manageAquariumData] = useState(AQUARIUMS)
+    
+    
+    const id = route.params.aquariumId
+    const newAquarium = route.params.newAquarium 
+
+    console.log(newAquarium)
+
+    const aquarium = AQUARIUMS.filter(aquarium => aquarium.id === id)[0];
+    // const addedAquarium = newAquarium.filter(addedAquarium => addedAquarium.id === newAquarium.id)[0];
+     
+    // {console.log(addedAquarium)}
+    
+    if (aquarium)
     return(
+        
         <View>
             <Card>
-                <Text>Hello World</Text>
+                <Text>{aquarium.name}</Text>
+                <Text>{aquarium.size}</Text>
+                <Text>Fish: {aquarium.fish}</Text>
+                <Text>Water Parameters</Text>
+                <Text>Temperature: {aquarium.temperature}</Text>
+                <Text>pH: {aquarium.pH}</Text>
+                <Text>Nitrate: {aquarium.nitrate}ppm</Text>
+                <Text>Nitrite: {aquarium.nitrite}ppm</Text>
+                <Text>Ammonia: {aquarium.ammonia}ppm</Text>
             </Card>
         </View>
+    )
+    else 
+    return(
+    <View>
+        <Card>
+            <Text>{newAquarium.name}</Text>
+            <Text>{newAquarium.size}</Text>
+           
+        </Card>
+    </View>
     )
 }
